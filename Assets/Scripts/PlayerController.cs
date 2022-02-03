1549002e2f1df5e54948fde3414ad4aca0ca6ec6
choisan32 <choisan32@gmail.com>
2022-02-03 14:51:52 -0500

    Made collectables disappear, made a count system, and win screen
    The collectables will now disappear when touched. Added a Counter at the corner of the screen to count the number of collectables touched. Added a win screen at the end of the game.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText; 
 
     private Rigidbody rb;
-    
+    private int count;
     private float movementX;
     private float movementY;
 
@@ -17,6 +19,9 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
     }
 
     void OnMove(InputValue movementValue)
@@ -27,10 +32,26 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
+        }
     }
 }
